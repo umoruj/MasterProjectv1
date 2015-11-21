9a75fa9238366fa67b5631343bb3927f34425f15
umoruj <umoru80@gmail.com>
2015-11-21 10:19:46 +0100

    Initial Commit

---
+++ MasterProjectv1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MasterProjectv1
+//
+//  Created by Umoru Joseph on 11/21/15.
+//  Copyright © 2015 Umoru Joseph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
